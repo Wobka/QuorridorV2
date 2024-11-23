f5b74442a2e2fc105747f787a360edc6fb400ca1
GabrielF1307 <gabriel.fleury2005@icloud.com>
2024-11-23 15:27:31 +0100

    Update score.h

--- score.h
+++ score.h
@@ -1,49 +1,12 @@
-#include <stdio.h>
-#include <string.h>
+//
+// Created by gabri on 23/11/2024.
+//
 
-#define MAX_JOUEURS 2
-#define TAILLE_NOM 50
+#ifndef CHARGER_SCORE_H
+#define CHARGER_SCORE_H
 
-typedef struct {
-    char nom[TAILLE_NOM];
-    int score;
-} Joueur;
+#endif
 
-int charger_scores(const char *scores, Joueur joueurs[], int *nb_joueurs) {
-    FILE *fichier = fopen(scores, "r");
-    if (fichier == NULL) {
-        printf("Aucun fichier trouvé. Un nouveau fichier sera créé.\n");
-        return 0;
-    }
-
-    while (fscanf(fichier, "%s %d", joueurs[*nb_joueurs].nom, &joueurs[*nb_joueurs].score) == 2) {
-        (*nb_joueurs)++;
-    }
-
-    fclose(fichier);
-    return 1;
-}
-
-
-void sauvegarder_scores(const char *nom_fichier, Joueur joueurs[], int nb_joueurs) {
-    FILE *fichier = fopen(nom_fichier, "w");
-    if (fichier == NULL) {
-        printf("Erreur : Impossible d'ouvrir le fichier pour écrire.\n");
-        return;
-    }
-
-    for (int i = 0; i < nb_joueurs; i++) {
-        fprintf(fichier, "%s %d\n", joueurs[i].nom, joueurs[i].score);
-    }
-
-    fclose(fichier);
-}
-
-int trouver_joueur(Joueur joueurs[], int nb_joueurs, const char *nom) {
-    for (int i = 0; i < nb_joueurs; i++) {
-        if (strcmp(joueurs[i].nom, nom) == 0) {
-            return i;
-        }
-    }
-    return -1;
-}
+int charger_scores();
+void sauvegarder_scores();
+int trouver_joueur();
