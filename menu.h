678bb9b8d83b64514eeaeed3fd89b3d0f1c5e226
GabrielF1307 <gabriel.fleury2005@icloud.com>
2024-11-25 15:33:10 +0100

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,15 @@
+#ifndef MENU_H
+#define MENU_H
+
+#endif //MENU_H
+
+void cursor(int lig, int col );
+
+void color(int couleurDuTexte,int couleurDeFond);
+
+int menu();
+void quitter();
+void afficherScores();
+void afficherAide();
+void reprendrePartie();
+void nouvellePartie();
