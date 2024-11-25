d6d6065fdbeed76a31f24d643ce8a6fb243ffde6
GabrielF1307 <gabriel.fleury2005@icloud.com>
2024-11-25 15:26:00 +0100

    Update score.h

--- score.h
+++ score.h
@@ -1,12 +1,16 @@
-//
-// Created by gabri on 23/11/2024.
-//
+#ifndef SCORE_H
+#define SCORE_H
 
+#endif //SCORE_H
 #ifndef CHARGER_SCORE_H
 #define CHARGER_SCORE_H
 
 #endif
-
+typedef struct {
+    char nom[50];
+    int score;
+} Joueur;
 int charger_scores();
 void sauvegarder_scores();
 int trouver_joueur();
+void score(int gagnant);
